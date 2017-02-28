fb116642ccac0b0e48ed9ac24337c0b1400539d6
seaman248 <semen.bko@ya.ru>
2017-02-28 20:35:46 +0700

    Prettify and add rules

--- ui.R
+++ ui.R
@@ -1,8 +1,20 @@
 library(shiny)
 
 shinyUI(fluidPage(
-    plotOutput('life_matrix'),
-    actionButton(label='Next step', inputId = 'stepButton'),
-    actionButton(label='Reset', inputId='reset'),
-    sliderInput('dim', 'Dim', min=5, max=50, value = 10)
+    tags$h1('Life Game'),
+    column(4, wellPanel(
+      sliderInput('dim', 'Dim', min=5, max=50, value = 10),
+      actionButton(label='Next step', inputId = 'stepButton'),
+      actionButton(label='Reset', inputId='reset'),
+      tags$h2('Rules:'),
+      tags$ol(
+        tags$li('Any live cell with fewer than two live neighbours dies, as if caused by underpopulation'),
+        tags$li('Any live cell with two or three live neighbours lives on to the next generation'),
+        tags$li('Any live cell with more than three live neighbours dies, as if by overpopulation'),
+        tags$li('Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction')
+      )
+    )),
+    column(6, 
+      plotOutput('life_matrix', width = 400, height = 400)
+    )
 ))
